c160a93520d82b60d13c50bd22e28fd2b7fbf836
Md. Mehedi Hasan <108575130+mehedidevs@users.noreply.github.com>
2023-08-24 19:39:45 +0600

    bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val core_version = "1.10.1"
+    implementation("androidx.core:core-ktx:$core_version")
 }
